9c9ff2a10fde9c3c4f19df8c29f2b2430ff704b0
bilal acikel <bilalackl@gmail.com>
2021-02-17 15:26:45 +0300

    initial

---
+++ stories/1-Icons.stories.js
@@ -0,0 +1,39 @@
+import React from "react";
+import * as Icons from "../components/icons";
+import Button from "../components/button";
+
+export default {
+  title: "Icons",
+};
+
+export const Icon = () => (
+  <div className="icons">
+    <Icons.ArrowBottom />
+    <Icons.Bookmark />
+    <Icons.BookmarkFill />
+    <Icons.Emoji />
+    <Icons.Explore />
+    <Icons.Gif />
+    <Icons.Home />
+    <Icons.HomeFill />
+    <Icons.Like />
+    <Icons.Lists />
+    <Icons.ListsFill />
+    <Icons.Media />
+    <Icons.Messages />
+    <Icons.MessagesFill />
+    <Icons.More />
+    <Icons.Notification />
+    <Icons.NotificationFill />
+    <Icons.Options />
+    <Icons.Profile />
+    <Icons.ProfileFill />
+    <Icons.Question />
+    <Icons.Reply />
+    <Icons.Retweet />
+    <Icons.Search />
+    <Icons.Share />
+    <Icons.TimelineProp />
+    <Icons.Twitter />
+  </div>
+);
